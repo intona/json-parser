93ae7021e84b112a7072b2c758e2630760f625c5
Vincent Lang <vincent.lang@intona.eu>
2020-03-25 17:42:24 +0100

    test: extend the stand-alone test program
    Originally this was created for JSONTestSuite. Add stuff that makes it
    vaguely useful for other purposes.

--- test/parser.c
+++ test/parser.c
@@ -1,8 +1,44 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "json.h"
+
+static void dump_tok(struct json_tok *tok, int depth)
+{
+    printf("%*s", depth, "");
+    switch (tok->type) {
+    case JSON_TYPE_NULL:
+        printf("null\n");
+        break;
+    case JSON_TYPE_BOOL:
+        printf("%s\n", tok->u.b ? "true" : "false");
+        break;
+    case JSON_TYPE_STRING:
+        printf("'%s'\n", tok->u.str);
+        break;
+    case JSON_TYPE_DOUBLE:
+        printf("%f\n", tok->u.d);
+        break;
+    case JSON_TYPE_ARRAY:
+        printf("[\n");
+        for (size_t n = 0; n < tok->u.array->count; n++)
+            dump_tok(&tok->u.array->items[n], depth + 2);
+        printf("%*s]\n", depth, "");
+        break;
+    case JSON_TYPE_OBJECT:
+        printf("{\n");
+        for (size_t n = 0; n < tok->u.object->count; n++) {
+            printf("%*s'%s':\n", depth, "", tok->u.object->items[n].key);
+            dump_tok(&tok->u.object->items[n].value, depth + 2);
+        }
+        printf("%*s}\n", depth, "");
+        break;
+    default:
+        printf("error\n");
+    }
+}
 
 static void json_msg_cb(void *opaque, size_t loc, const char *msg)
 {
@@ -11,22 +47,62 @@ static void json_msg_cb(void *opaque, size_t loc, const char *msg)
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        goto error_args;
+    char *arg = NULL;
+    bool is_file = true;
+    bool dump = false;
 
-    FILE *f = fopen(argv[1], "rb");
-    if (!f)
-        goto error_file;
+    for (int n = 1; n < argc; n++) {
+        if (strcmp(argv[n], "--string") == 0) {
+            is_file = false;
+        } else if (strcmp(argv[n], "--dump") == 0) {
+            dump = true;
+        } else if (argv[n][0] == '-') {
+            fprintf(stderr, "Invalid command line arguments.\n");
+            fprintf(stderr, "   --string    Argument is a string to parse.\n");
+            fprintf(stderr, "   --dump      Dump JSON tree.\n");
+            fprintf(stderr, "   arg         File, or JSON string if --string.\n");
+            return 2;
+        } else {
+            if (arg) {
+                fprintf(stderr, "Only one non-option argument expected.\n");
+                return 2;
+            }
+            arg = argv[n];
+        }
+    }
 
-    if (fseek(f, 0, SEEK_END) < 0)
-        goto error_file;
+    if (!arg) {
+        fprintf(stderr, "%s argument expected.\n", is_file ? "file" : "string");
+        return 2;
+    }
 
-    long size = ftell(f);
-    if (size < 0)
-        goto error_file;
+    long size = 0;
+    void *data = NULL;
+    if (is_file) {
+        FILE *f = fopen(argv[1], "rb");
+        if (!f)
+            goto error_file;
 
-    if (fseek(f, 0, SEEK_SET) < 0)
-        goto error_file;
+        if (fseek(f, 0, SEEK_END) < 0)
+            goto error_file;
+
+        long size = ftell(f);
+        if (size < 0)
+            goto error_file;
+
+        if (fseek(f, 0, SEEK_SET) < 0)
+            goto error_file;
+
+        void *data = calloc(1, size + 1);
+        if (!data)
+            goto error_size;
+
+        if (size && fread(data, size, 1, f) != 1)
+            goto error_file;
+    } else {
+        size = strlen(arg);
+        data = arg;
+    }
 
     // Estimate "some" memory size to hold the result. Normally, you'd provide a
     // fixed buffer size (embedded scenario), or modify the parser to use malloc
@@ -38,28 +114,17 @@ int main(int argc, char **argv)
     if (!memory)
         goto error_size;
 
-    void *data = calloc(1, size + 1);
-    if (!data)
-        goto error_size;
-
-    if (size && fread(data, size, 1, f) != 1)
-        goto error_file;
-
-
-
     struct json_msg_cb cb = {json_msg_cb};
     struct json_tok *tok = json_parse(data, memory, memory_size, 1000, &cb);
     if (tok) {
         printf("Success.\n");
+        if (dump)
+            dump_tok(tok, 0);
         return 0;
     } else {
         fprintf(stderr, "Parsing failed.\n");
         return 1;
     }
-
-error_args:
-    fprintf(stderr, "Invalid command line arguments.\n");
-    return 2;
 
 error_file:
     fprintf(stderr, "could not open or read file.\n");
